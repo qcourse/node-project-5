75c4f5b904901a8a115b77e3e720b27fa1e0b921
linktang <tomleader4444@gmail.com>
2016-07-12 16:19:17 +0800

    Update upload.js

--- module/upload.js
+++ module/upload.js
@@ -3,6 +3,8 @@
 const path = require("path");
 const fs = require("fs");
 const multer = require("multer");
+const YOUTU_SDK = require('../lib/youtu');
+const bucketName = YOUTU_SDK.bucketName;
 const destPath = './upload/';
 
 function upload(request, response) {
@@ -29,8 +31,26 @@ function upload(request, response) {
         console.log("#1. File uploaded to server:");
         console.log(JSON.stringify(file));
 
+        uploadToYoutu(file);
     }
 
-    
+    //通过优图SDK把文件上传到优图bucket
+    function uploadToYoutu(file) {
+
+        console.log('#2. begin upload to Youtu');
+        const uploadPath = `/uploads/${file.originalname}`;
+        YOUTU_SDK.upload(file.path, bucketName, uploadPath, (result) => {
+
+            console.log('#3. upload to Youtu done, please check result');
+            console.log(JSON.stringify(result));
+
+            response.json(result);
+
+            //服务器上的文件删掉
+            fs.unlink(file.path);
+
+
+        });
+    }
 }
 module.exports = upload;
